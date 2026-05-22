0b16e13a14df8782977515fb8d906efdd331d2e3
vanichols <virginia.nichols@gmail.com>
2026-05-22 14:04:47 +0200

    added new sensor to plot info

--- data-raw/keys/create-soilsensorkey.R
+++ data-raw/keys/create-soilsensorkey.R
@@ -2,20 +2,24 @@ library(tidyverse)
 
 #--for the 24/25 season, these sensor ids were assigned to these plots
 
-load("data/op_plotkey.rda")
+load("data/op_eukey.rda")
 load("data/op_trtkey.rda")
+load("data/op_envkey.rda")
+op_envkey
+
+# sexy1 season 24/25 ------------------------------------------------------
 
 #--only put sensors in p and a trts
-d1 <-
-  op_plotkey |>
+a1 <-
+  op_eukey |>
   filter(env_key == "0101") |> #--sexy1 season 24/25
-  select(-plot_key) |>
+  select(-eu_key) |>
   left_join(op_trtkey |> select(env_key, trt_key, trt_id)) |>
   select(-trt_key) |>
   filter(trt_id %in% c("p", "a"))
 
-d2 <-
-  d1 |>
+a2 <-
+  a1 |>
   mutate(soilsensor_id = case_when(
   plot_id == "101" ~ "94243864",
   plot_id == "105" ~ "94243868",
@@ -28,9 +32,45 @@ d2 <-
   TRUE ~ "0000000"
   ))
 
-d2
+a2
+
+# sexy2 season 25/26 ------------------------------------------------------
+
+op_envkey
+
+b1 <-
+  op_eukey |>
+  filter(env_key == "0202") |> #--sexy1 season 24/25
+  select(-eu_key) |>
+  left_join(op_trtkey |> select(env_key, trt_key, trt_id)) |>
+  select(-trt_key) |>
+  filter(trt_id %in% c("p", "a"))
 
 
-op_soilsensorkey <- d2
+b2 <-
+  b1 |>
+  mutate(soilsensor_id = case_when(
+    plot_id == "102" ~ "94243868",
+    plot_id == "112" ~ "94243865",
+    plot_id == "210" ~ "94243864",
+    plot_id == "211" ~ "94243869",
+    plot_id == "303" ~ "94243862",
+    plot_id == "308" ~ "94243866",
+    plot_id == "402" ~ "94243861",
+    plot_id == "409" ~ "94243867",
+    TRUE ~ "0000000"
+  ))
+
+b2
+
+
+
+# put together ------------------------------------------------------------
+
+data <-
+  a2 |>
+  bind_rows(b2)
+
+op_soilsensorkey <- data
 
 usethis::use_data(op_soilsensorkey, overwrite = T)
--- data-raw/keys/create_envkey.R
+++ data-raw/keys/create_envkey.R
@@ -20,10 +20,15 @@ a1 <-
   "01",         "sexy1",   "flakkebjerg",  "22",
   "02",         "sexy2",   "flakkebjerg",  "06",
   "03",         "seed1",   "flakkebjerg",  "19",
-  "04",         "cry1",   "flakkebjerg",   "TBD")
+  "04",         "climax1",   "flakkebjerg",   "37")
 
 op_lockey <-
   a1
+
+usethis::use_data(op_lockey, overwrite = TRUE)
+
+op_lockey %>%
+  write_csv("inst/extdata/op_lockey.csv")
 
 
 # growing season key ------------------------------------------------------------
@@ -43,17 +48,29 @@ b1 <-
 op_seakey <-
   b1
 
+usethis::use_data(op_seakey, overwrite = TRUE)
+
+op_seakey %>%
+  write_csv("inst/extdata/op_seakey.csv")
 
 # combine -----------------------------------------------------------------
+#--this creates envkeys that aren't real though...
 
-op_envkey <-
+c1 <-
   crossing(op_seakey |> select(sea_key),
          op_lockey |> select(loc_key)) |>
   unite(loc_key, sea_key, col = "env_key", sep = "", remove = F) |>
   left_join(op_seakey) |>
-  left_join(op_lockey)
+  left_join(op_lockey) |>
+  select(env_key, loc_key, sea_key, everything())
+
+op_envkey <-
+  c1 |>
+  filter(!(env_key == "0201"), #--no sexy2 in 24/25
+         !(env_key == "0401") #--no climax1 in 24/25
+         )
 
 usethis::use_data(op_envkey, overwrite = TRUE)
 
-op_seakey %>%
+op_envkey %>%
   write_csv("inst/extdata/op_envkey.csv")
